e7134c5f3258080b67b67ce60e71cb28bbdee68f
Ragad <ragadkaraman22@gmail.com>
2026-03-23 03:16:37 +0300

    chart.js grafikleri eklendi ve Yorum index eklendi

--- repos/FitAl/FitAl/Controllers/AiTalimatController.cs
+++ repos/FitAl/FitAl/Controllers/AiTalimatController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 public class AiTalimatController : Controller
 {
     public IActionResult Index() => View();
+    public IActionResult Gecmis() => View();
 
     [HttpPost]
     public IActionResult Gonder(string talimatMetni) => RedirectToAction("Index");
--- repos/FitAl/FitAl/Controllers/UrunController.cs
+++ repos/FitAl/FitAl/Controllers/UrunController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 public class UrunController : Controller
 {
     public IActionResult Index() => View();
-
+    public IActionResult Ekle() => View();
+    public IActionResult Analiz() => View();
     public IActionResult Detay(int id) => View();
 }
