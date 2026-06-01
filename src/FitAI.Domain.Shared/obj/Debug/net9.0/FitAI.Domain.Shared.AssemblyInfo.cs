faa5a1782ca05d6f1f66a9b282e9e0a4a109dab1
Ragad <ragadkaraman22@gmail.com>
2026-06-01 23:09:49 +0300

    düzeltmeler

--- src/FitAI.Domain.Shared/obj/Debug/net9.0/FitAI.Domain.Shared.AssemblyInfo.cs
+++ src/FitAI.Domain.Shared/obj/Debug/net9.0/FitAI.Domain.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitAI.Domain.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1cf541578201707cacd15ea65256ee02d4712a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f21136c246733e6a8fe8bda6c4b7a042fd3ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitAI.Domain.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitAI.Domain.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
